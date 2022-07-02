de1b99aaa4d67b57e2fd0916db737d3248fa87fa
Giutear <gsawczak@gmx.de>
2022-07-02 20:21:54 +0200

    Finished graph attention

--- attention.py
+++ attention.py
@@ -26,14 +26,14 @@ class cRPEncoding(nn.Module):
             for _ in range(3 * num_heads)
         ])
 
-    def forward(self, xyz: torch.Tensor):
-        rel_xyz = (xyz.unsqueeze(1) - xyz.unsqueeze(0)).reshape(-1, 3)
+    def forward(self, xyz: torch.Tensor, edges: torch.LongTensor):
+        rel_xyz = (xyz.unsqueeze(-2) - xyz[edges]).reshape(-1, 3)
 
         return tuple(
             torch.cat(tuple(self.encoders[i * self.num_heads + h](
-                rel_xyz).reshape(1, xyz.shape[0], xyz.shape[0], -1)
+                rel_xyz).reshape(xyz.shape[0], 1, edges.shape[1], -1)
                             for h in range(self.num_heads)),
-                      dim=0) for i in range(3))
+                      dim=1) for i in range(3))
 
 
 class ContextMultiheadAttention(nn.Module):
@@ -63,26 +63,26 @@ class ContextMultiheadAttention(nn.Module):
         self.kv = nn.Linear(self.total_dim, 2 * self.total_dim, bias=use_bias)
         self.proj = nn.Linear(self.total_dim, self.total_dim, bias=use_bias)
 
-    def forward(self, xyz: torch.Tensor, feat: torch.Tensor):
-        q_enc, k_enc, v_enc = self.encoding(xyz)
+    def forward(self, xyz: torch.Tensor, feat: torch.Tensor, edges):
+        k = edges.shape[1]
+
+        q_enc, k_enc, v_enc = self.encoding(xyz, edges)
         k_enc: torch.Tensor
-        queries = self.q(feat).reshape(self.num_heads, -1, self.dimension)
+        queries = self.q(feat).reshape(-1, self.num_heads, 1, self.dimension)
         kv: torch.Tensor = self.kv(feat)
-        keys = kv[:, :self.total_dim].reshape(self.num_heads, -1,
-                                              self.dimension)
-        values = kv[:, self.total_dim:].reshape(self.num_heads, -1,
-                                                self.dimension)
+        keys = kv[:, :self.total_dim][edges].reshape(-1, k, self.num_heads,
+                                                     self.dimension).swapaxes(
+                                                         -2, -3)
+        values = kv[:, self.total_dim:][edges].reshape(
+            -1, k, self.num_heads, self.dimension).swapaxes(-2, -3)
 
         attention = torch.matmul(queries, keys.swapaxes(-1, -2))
 
-        attention += torch.matmul(queries.unsqueeze(-2),
-                                  q_enc.swapaxes(-1, -2)).squeeze()
+        attention += torch.matmul(queries, q_enc.swapaxes(-1, -2))
         attention += torch.matmul(keys.unsqueeze(-2),
-                                  k_enc.permute(0, 2, 3,
-                                                1)).squeeze().swapaxes(-1, -2)
+                                  k_enc.unsqueeze(-1)).squeeze().unsqueeze(-2)
 
-        scores = torch.softmax(attention, dim=-1).unsqueeze(-1)
+        scores = torch.softmax(attention, dim=-1).swapaxes(-1, -2)
 
-        y = ((values.unsqueeze(1) + v_enc) * scores).sum(dim=2).swapaxes(
-            0, 1).reshape(-1, self.total_dim)
+        y = ((values + v_enc) * scores).sum(dim=2).reshape(-1, self.total_dim)
         return self.proj(y)
--- transformer.py
+++ transformer.py
@@ -21,9 +21,10 @@ class ContextTransformer(nn.Module):
         self.post_transform = nn.ModuleList(
             [nn.LayerNorm(f_dim), nn.Linear(f_dim, f_dim)])
 
-    def forward(self, xyz: torch.Tensor, feat: torch.Tensor):
+    def forward(self, xyz: torch.Tensor, feat: torch.Tensor,
+                edges: torch.LongTensor):
         feat = self.pre_norm(feat)
-        feat = feat + self.attention(xyz, feat)
+        feat = feat + self.attention(xyz, feat, edges)
         _feat = feat
         for layer in self.post_transform:
             _feat = layer(_feat)
